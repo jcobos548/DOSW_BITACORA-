cdbe5cd82ca53daff1187d974f6f6e3119937f8b
Julian Giral <Julian.giral-c@mail.escuelaing.edu.co>
2026-08-31 22:33:53 -0500

    Resuelve ejercicio 7 de Pokemon

---
+++ src/main/java/dosw/semana_2/pokemon/Ejercicio7.java
@@ -0,0 +1,24 @@
+package dosw.semana_2.pokemon;
+
+import java.util.List;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+
+        List<String> pokemones = List.of(
+                "Squirtle",
+                "Pikachu",
+                "Mewtwo",
+                "Bulbasaur",
+                "Charmander",
+                "Abra"
+        );
+
+        List<String> pokemonesOrdenados = pokemones.stream()
+                .sorted()
+                .toList();
+
+        System.out.println(pokemonesOrdenados);
+    }
+}
