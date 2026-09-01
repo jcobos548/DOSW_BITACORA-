57dd214adbc9040e3495a9f74550ced935b72d8f
Julian Giral <Julian.giral-c@mail.escuelaing.edu.co>
2026-08-31 23:09:44 -0500

    Resuelve ejercicio 16 de Pokemon

---
+++ src/main/java/dosw/semana_2/pokemon/Ejercicio16.java
@@ -0,0 +1,27 @@
+package dosw.semana_2.pokemon;
+
+import java.util.List;
+
+public class Ejercicio16 {
+
+    public static void main(String[] args) {
+
+        List<Entrenador> entrenadores = List.of(
+                new Entrenador(1L, "Ash", 8, List.of()),
+                new Entrenador(2L, "Misty", 5, List.of()),
+                new Entrenador(3L, "Brock", 6, List.of()),
+                new Entrenador(4L, "Gary", 10, List.of()),
+                new Entrenador(5L, "May", 3, List.of()),
+                new Entrenador(6L, "Dawn", 7, List.of())
+        );
+
+        List<String> experimentados = entrenadores.stream()
+                .filter(entrenador -> entrenador.getMedallas() > 5)
+                .map(entrenador ->
+                        entrenador.getNombre() + "(" + entrenador.getMedallas() + ")")
+                .toList();
+
+        System.out.println("Entrenadores con > 5 medallas:");
+        System.out.println(experimentados);
+    }
+}
