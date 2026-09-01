fcbb0377c3f195254353acf717af708623a415a1
Julian Giral <Julian.giral-c@mail.escuelaing.edu.co>
2026-08-31 22:54:11 -0500

    Resuelve ejercicio 12 de Pokemon

---
+++ src/main/java/dosw/semana_2/pokemon/Ejercicio12.java
@@ -0,0 +1,26 @@
+package dosw.semana_2.pokemon;
+
+import java.util.List;
+import java.util.Comparator;
+
+public class Ejercicio12 {
+
+    public static void main(String[] args) {
+
+        List<Pokemon> pokemones = List.of(
+                new Pokemon(1L, "Pikachu", "Eléctrico", 45, 320, "Kanto", false),
+                new Pokemon(2L, "Mewtwo", "Psíquico", 88, 680, "Kanto", true),
+                new Pokemon(3L, "Dragonite", "Dragón", 82, 530, "Kanto", false),
+                new Pokemon(4L, "Charizard", "Fuego", 62, 610, "Kanto", false)
+        );
+
+        Pokemon campeon = pokemones.stream()
+                .max(Comparator.comparingDouble(Pokemon::getPoderCombate))
+                .orElseThrow();
+
+        System.out.println(
+                "Campeón: " + campeon.getNombre()
+                        + " con PC: " + (int) campeon.getPoderCombate()
+        );
+    }
+}
